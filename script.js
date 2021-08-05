7007aed0a712046f456cf56d11bf9a65eb0c6c09
jmvt717 <jmvt717@josephmilton.world>
2021-08-05 18:07:09 -0500

    added prompt windows for upper, lowercase, numbers and special characters

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 let upper = ["abcdefghijklmnopqrstuvwxyz".split];
 let lower = ["abcdefghijklmnopqrstuvwxyz".split];
-let chosenValues = [];
+let passwordLegnth = [];
 
 
 function generatePassword(){
-  let sign = prompt("Password Length?");
-  // if (sign.
-
-
-
-
-  // return "test";
+  
+  passwordLegnth = parseInt(prompt("Password Length? Choose between 8 and 128 characters."));
+    result = window.confirm("include Lowercase?");
+    if (window.confirm("Include Upercase?")) {
+      window.confirm("Include Numbers?");
+      window.confirm("Include Special Characters?");
+    } 
+    return = password
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
