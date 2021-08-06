eed79fb7dc49d86a2c798a5d27e9d2906c4161e4
jmvt717 <jmvt717@josephmilton.world>
2021-08-05 21:47:42 -0500

    added upper, lower, special, and number variables.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let upper = ["abcdefghijklmnopqrstuvwxyz".split];
-let lower = ["abcdefghijklmnopqrstuvwxyz".split];
-let passwordLegnth = [];
+let upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let special = ["!","@","#","$","%","^","&","*","?","/"]
+let number = ["0","1","2","3","4","5","6","7","8","9"]
+let chosenValue =[]
 
 
 function generatePassword(){
