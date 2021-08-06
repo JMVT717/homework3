2b09d912fa9e9ed78a67965bf978da15ec47ad6c
jmvt717 <jmvt717@josephmilton.world>
2021-08-05 22:22:14 -0500

    far as I could get, no password generated :(

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q"
 let lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let special = ["!","@","#","$","%","^","&","*","?","/"]
 let number = ["0","1","2","3","4","5","6","7","8","9"]
-let chosenValue =[]
+let chosenValue =[];
 
 
 function generatePassword(){
@@ -15,7 +15,6 @@ function generatePassword(){
       window.confirm("Include Numbers?");
       window.confirm("Include Special Characters?");
     } 
-    return = password
 }
 
 // Write password to the #password input
